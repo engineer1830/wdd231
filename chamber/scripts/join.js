04157a848f3cd7dbf7133f1ca3debbb133cb74c2
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-05-25 16:54:12 -0700

    push

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const dialogBoxText = document.getElementById("dialogBoxText");
 
     const modalText = {
-        nonprofit: "NP Membership is for non profit organizations and there is no fee (np).",
+        nonprofit: "NP Membership is for non-profit organizations and there is no fee (np).",
         bronze: "Bronze membership is the basic member level and the business is included in the member directory.",
         silver: "Silver membership is the 2nd highest tier of membership and includes spotlight features on the Chamber's Homepage.",
         gold: "Gold membership is the top tier of membership and includes spotlight features on the Chamber's Homepage and inclusion on the chamber board."
