4690a62c80791af42b2f80320526c1891fabdd50
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-05-25 22:00:05 -0700

    push

--- project/scripts/sectordata.js
+++ project/scripts/sectordata.js
@@ -13,16 +13,19 @@ export function displaySectors(sectors) {
         const card = document.createElement("section");
         card.classList.add("sector-card");
 
+        const sectorHeader = document.createElement("h3");
+        sectorHeader.classList.add("sector-header");
+
         const sectorIcon = document.createElement("img");
         sectorIcon.src = sector.icon;
         sectorIcon.alt = `${sector.sectorName} icon`;
         sectorIcon.loading = "lazy";
-        sectorIcon.width = 80;
+        sectorIcon.width = 40;
 
-        const sectorName = document.createElement("h2");
+        const sectorName = document.createElement("h3");
         sectorName.textContent = sector.sectorName;
 
-        const sectorIndustries = document.createElement("h3");
+        const sectorIndustries = document.createElement("h4");
         sectorIndustries.textContent = `Industries: ${sector.industries.join(", ")}`;
 
         const riskProfile = document.createElement("h4");
@@ -41,8 +44,8 @@ export function displaySectors(sectors) {
         sectorDrivers.textContent = `Key Drivers: ${sector.keyDrivers.join(", ")}`;
 
 
-        card.appendChild(sectorIcon);
-        card.appendChild(sectorName);
+        sectorHeader.append(sectorIcon, sectorName);
+        card.appendChild(sectorHeader);
         card.appendChild(sectorIndustries);
         card.appendChild(riskProfile);
         card.appendChild(sectorDescription);
