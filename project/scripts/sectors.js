253e32c79229d96798b9f7cde3572d769b0d8f43
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-05-26 11:36:09 -0700

    push

--- project/scripts/sectors.js
+++ project/scripts/sectors.js
@@ -2,10 +2,45 @@ import { getSectorsData, displaySectors, displayMacroGroups } from "./sectordata
 
 document.addEventListener("DOMContentLoaded", async () => {
     const directory = document.querySelector("#directory");
-    
-
     const { sectors, macroGroups } = await getSectorsData();
 
-    displayMacroGroups(macroGroups);
-    displaySectors(sectors);
+    const allBtn = document.querySelector("#allBtn");
+    const macroBtn = document.querySelector("#macroBtn");
+    const sectorBtn = document.querySelector("#sectorBtn");
+
+
+    function render(type) {
+        directory.innerHTML = "";
+
+        if (type === "all") {
+            displayMacroGroups(macroGroups);
+            displaySectors(sectors);
+        } else if (type === "macro") {
+            displayMacroGroups(macroGroups);
+        } else if (type === "sector") {
+            displaySectors(sectors);
+        }
+    }
+
+    function setActive(button) {
+        [allBtn, macroBtn, sectorBtn].forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+    }
+
+    allBtn.addEventListener("click", () => {
+        setActive(allBtn);
+        render("all");
+    });
+
+    macroBtn.addEventListener("click", () => {
+        setActive(macroBtn);
+        render("macro");
+    });
+
+    sectorBtn.addEventListener("click", () => {
+        setActive(sectorBtn);
+        render("sector");
+    });
+
+    render("all");
 });
