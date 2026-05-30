c0e342d691b80da5f5f1a5265cee91d8ace89f41
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-05-29 20:16:16 -0700

    push

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,6 +1,6 @@
 import { getActivityData, displayActivities } from "./activity.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const { activities } = await getActivityData();
+    const {activities} = await getActivityData();
     displayActivities(activities);
 });
