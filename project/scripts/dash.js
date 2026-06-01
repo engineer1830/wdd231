2a655dce28380035c5aad8e955c5e59d23937cf4
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-05-31 21:23:38 -0700

    push

--- project/scripts/dash.js
+++ project/scripts/dash.js
@@ -57,9 +57,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const dialogBox = document.getElementById("dialogBox");
     const dialogBoxText = document.getElementById("dialogBoxText");
+    const openModalBtn = document.getElementById("openModalBtn");
+    const closeButton = document.getElementById("closeButton");
 
     const modalText = "This dashboard is representative only.It is built using only free APIs and as such, not all financial fundamentals are available.These calculations are based on the cumulative performance of the top 15 stocks in each sector and comparison to the broader market segments will have variations.";
 
+    dialogBoxText.textContent = modalText;
+
+    openModalBtn.addEventListener("click", () => {
+        dialogBox.showModal();
+    });
 
     document.getElementById("closeButton").addEventListener("click", () => {
         dialogBox.close();
