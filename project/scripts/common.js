7bf0faa97fdbe5f8c183a32d38f92ecc1b02e396
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-06-02 11:03:12 -0700

    push

---
+++ project/scripts/common.js
@@ -0,0 +1,6 @@
+// Footer year
+const year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = year;
+
+// Last modified
+document.getElementById("lastModified").innerHTML = document.lastModified;
