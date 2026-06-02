28476135f2a3c0725a98564b93f9f38a71cf9dbf
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-06-01 17:58:49 -0700

    push

--- project/scripts/stockdetail.js
+++ project/scripts/stockdetail.js
@@ -128,11 +128,11 @@ export async function getStockDetail(ticker) {
 //     renderStockDetails(details);
 // });
 
-document.getElementById("detailsBtn").addEventListener("click", async () => {
-    console.log(">>> CLICK HANDLER FIRED");
-    const ticker = document.getElementById("tickerInput").value.toUpperCase();
-    const details = await getStockDetail(ticker);
-    console.log(">>> DETAILS RETURNED:", details);
-    renderStockDetails(details);
-});
+// document.getElementById("detailsBtn").addEventListener("click", async () => {
+//     console.log(">>> CLICK HANDLER FIRED");
+//     const ticker = document.getElementById("tickerInput").value.toUpperCase();
+//     const details = await getStockDetail(ticker);
+//     console.log(">>> DETAILS RETURNED:", details);
+//     renderStockDetails(details);
+// });
 
